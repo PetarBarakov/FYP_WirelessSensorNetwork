9757b2b42bc486b9bce6f3ccd580ed06eef12994
Petar Barakov <barakov.petar@gmail.com>
2024-04-18 08:45:02 +0100

    Add configration functions for the mode and FIFO registers

--- lib/MAX30102/MAX30102.cpp
+++ lib/MAX30102/MAX30102.cpp
@@ -1,7 +1,92 @@
 #include "MAX30102.h"
 
-MAX30102::MAX30102 (uint8_t sensorAddress) : Sensor(sensorAddress)
+void MAX30102::fifoConfig(uint8_t sampleAverage)
 {
+    //Setup the FIFO configuration
+
+    //Chose a sampling average:
+    /*  The possible averages of the fifo samples are:
+    0b000                     -->        1  sample
+    0b001                     -->        2  samples
+    0b010                     -->        4  samples
+    0b011                     -->        8  samples
+    0b100                     -->        16 samples
+    0b101, 0b110, 0b111       -->        32 samples
+    */
+
+    uint8_t fifoAFullBits = 0b0000; //Set the FIFO almost full value 
+    bool fifoROBit = 0b1;           //Set the FIFO roll over
+    uint8_t smpAvgBits = 0b011;     //default averaging of 8 samples;
+
+    switch (sampleAverage)
+    {
+    case 1:
+        smpAvgBits = 0b000;
+        break;
+    
+    case 2:
+        smpAvgBits = 0b001;
+        break;
+    
+    case 4:
+        smpAvgBits = 0b010;
+        break;
+    
+    case 8:
+        smpAvgBits = 0b011;
+        break;
+    
+    case 16:
+        smpAvgBits = 0b100;
+        break;
+    
+    case 32:
+        smpAvgBits = 0b101;
+        break;
+    
+    default:
+        break;
+    }
+
+    //Calculate and write the register value
+    uint8_t fifoConfigBits = fifoAFullBits + 16 * fifoROBit + 32 * smpAvgBits;
+    writeToReg(FIFO_CONFIG, fifoConfigBits);
+}
+
+void MAX30102::modeConfig(uint8_t mode)
+{
+    //Setup the mode of measurements
+
+    /*  Available modes:
+    Mode 0:      0b010       -->         Heart Rate mode (Red LED only)
+    Mode 1:      0b011       -->         SpO2 mode (Red and IR LEDs)
+    Mode 2:      0b111       -->         Multi-LED mode (Red and IR LEDs)
+    */
+
+    uint8_t modeBits = 0b011; //default mode is Sp02
+    
+    if(mode == 0) modeBits = 0b010;
+    else if(mode == 2) modeBits = 0b111;
+
+    writeToReg(MODE_CONFIG, modeBits);
+
 
 }
 
+MAX30102::MAX30102 (uint8_t sensorAddress,
+                    uint8_t sampleAverage,
+                    uint8_t mode
+                    ) : Sensor(sensorAddress)
+{
+    fifoConfig(sampleAverage);
+    modeConfig(mode);
+}
+
+void MAX30102::clearFIFO()
+{
+    //Loose all data currently stored in the FIFO
+
+    writeToReg(FIFO_WR_PTR, 0x00);
+    writeToReg(FIFO_RD_PTR, 0x00);
+    writeToReg(FIFO_OVF_COUNTER, 0x00);
+}
--- lib/MAX30102/MAX30102.h
+++ lib/MAX30102/MAX30102.h
@@ -5,24 +5,44 @@
 #include <Wire.h>
 #include "SensorClass.h"
 
+// Interupt Registers
+// All interupts are disabled by default
+// #define INT_STAT1 0x00
+// #define INT_STAT2 0x01
+// #define INT_EN1 0x02
+// #define INT_EN2 0x03
+
+//FIFO Register
+#define FIFO_WR_PTR 0x04
+#define FIFO_OVF_COUNTER 0x05
+#define FIFO_RD_PTR 0x06
+#define FIFO_DATA 0x07
+
 // Main Configuration Register Addresses
 #define FIFO_CONFIG 0x08
 #define MODE_CONFIG 0x09
 #define SPO2_CONFIG 0x0A
-// TODO: Add LED configs and MultiLedControl
+#define LED1_PA 0x0C    //RED LED
+#define LED2_PA 0x0D    //IR LED
+
 
 
 
 class MAX30102 : public Sensor
 {
     public:
-    MAX30102(uint8_t sensorAddress);
+    MAX30102(uint8_t sensorAddress, uint8_t sampleAverage, uint8_t mode);
 
     void SPO2read();
-    void HRread();
+    void HRread();  
 
     private:
     void readFIFO();
+    void clearFIFO();
+
+    //Configiration functions
+    void fifoConfig(uint8_t sampleAverage);
+    void modeConfig(uint8_t mode);
 
 };
 
