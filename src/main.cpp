f1e10cfadfd452fb71bfb93ba5ef81b9622676a5
Petar Barakov <barakov.petar@gmail.com>
2024-05-04 16:52:00 +0100

    initial test of INA236. Code uploads and provides untested data

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 #define CURRENT_SENSE_ESP_ADDRESS 0x40  //Large Resolution and ADC Range
 #define CURRENT_SENSE_SENSORS_ADDRESS 0x45  //Large Resolution and ADC Range
 
+#define INPUT_CURRENT_SENSE_RESISTANCE 130 //Resistor for the input power measurements
+
 
 INA236 InputcurrentSense(CURRENT_SENSE_INPUT_ADDRESS);
 
@@ -26,15 +28,27 @@ void setup() {
   Wire.setPins(pinSDA, pinSCL);
   Wire.begin();
 
-  InputcurrentSense.init(LARGE_ADCRANGE, 64, 130);
+  InputcurrentSense.init(LARGE_ADCRANGE, 64, INPUT_CURRENT_SENSE_RESISTANCE);
 
 }
 
 void loop() {
   // I2CSearchAddr();
-  double ShuntVoltage = 0;
-  InputcurrentSense.readShuntVoltage(ShuntVoltage);
-  Serial.printf("Shunt Voltage from sensor 1: %f\n", ShuntVoltage);
+  double shuntVoltage = 0;
+  uint16_t busVoltage = 0;
+  uint16_t power = 0;
+  double current = 0;
+
+  InputcurrentSense.readShuntVoltage(shuntVoltage);
+  InputcurrentSense.readBusVoltage(busVoltage);
+  InputcurrentSense.readPower(power);
+  InputcurrentSense.readCurrent(current);
+
+  Serial.printf("Shunt Voltage from sensor 1: %f\n", shuntVoltage);
+  Serial.printf("Bus Voltage from sensor 1: %d\n", busVoltage);
+  Serial.printf("Power from sensor 1: %d\n", power);
+  Serial.printf("Current from sensor 1: %f\n", current);
+  Serial.println();
   
   delay(2000);
 }
