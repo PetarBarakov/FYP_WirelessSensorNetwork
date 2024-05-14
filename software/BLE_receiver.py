a3a7a065dc4fe0b8c8b80c7bd44358dcbaacb275
Petar Barakov <barakov.petar@gmail.com>
2024-05-14 11:36:43 +0100

    Live plotting and BLE data colection (working)

--- software/BLE_receiver.py
+++ software/BLE_receiver.py
@@ -3,6 +3,7 @@ from bleak import BleakScanner, BleakClient
 
 # import pandas as pd
 from time import sleep
+import matplotlib.pyplot as plt
 
 # import liveplot_test
 
@@ -10,7 +11,7 @@ SERVICE_UUID        = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 DeviceName = "FYP_SensorNode0"
-samplingRate = 2 # 2 second sampling rate
+samplingRate = 0.1 # 2 second sampling rate
 
 Temp = 0
 Humid = 0
@@ -25,12 +26,6 @@ async def BLEconnect():
         print("DEVICE NOT FOUND.....")
     return device
 
-async def TempHumid():
-    global Humid, Temp
-    while True:
-        await asyncio.sleep(samplingRate)
-        print(f"1:Temperature: {Temp} \t Humidity: {Humid}")
-
 async def BLErx(device, outputFile):
     global Humid, Temp
     if device:
@@ -43,27 +38,26 @@ async def BLErx(device, outputFile):
                 Temp = float(TempHumid.split(",")[0])
                 Humid = float(TempHumid.split(",")[1])
 
-                print(f"2:Temperature: {Temp} \t Humidity: {Humid}")
+                print(f"Temperature: {Temp} \t Humidity: {Humid}")
     
                 outputFile.write(TempHumid)
                 outputFile.write("\n")
                 await asyncio.sleep(samplingRate)
 
-                # return Temp, Humid
 
-async def async_main(device, outputFile):
+# async def async_main(device, outputFile):
     
-    await asyncio.gather(BLErx(device, outputFile), TempHumid())
+#     await asyncio.gather(BLErx(device, outputFile), plot())
 
 
     
 
-if __name__ == "__main__":
-    DeviceDetected = asyncio.run(BLEconnect())
+# if __name__ == "__main__":
+#     DeviceDetected = asyncio.run(BLEconnect())
 
-    data_file = open("Data/output.csv", "w")
-    data_file.write("Temperature, Humidity\n")
+#     data_file = open("Data/output.csv", "w")
+#     data_file.write("Temperature, Humidity\n")
 
-    asyncio.run(async_main(device=DeviceDetected, outputFile=data_file))
+#     asyncio.run(async_main(device=DeviceDetected, outputFile=data_file))
 
-    data_file.close()
+#     data_file.close()
--- software/liveplot_test.py
+++ software/liveplot_test.py
@@ -2,51 +2,21 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 import BLE_receiver
+import asyncio
 
+import time
 
 import random
 
-# Parameters
-timeLen = 200         # Number of points to display
-tempRange = [10, 40]  # Range of possible Y values to display
-humidRange = [15, 30]  # Range of possible Y values to display
+import threading
 
-# Create figure for plotting
-fig = plt.figure()
-tempax = fig.add_subplot(1, 1, 1)
-humidax = tempax.twinx()
-timeSamples = list(range(0, 200))
-tempSamples = [0] * timeLen
-humidSamples = [0] * timeLen
-tempax.set_ylim(tempRange)
-humidax.set_ylim(humidRange)
 
-# Create a blank line. We will update the line in animate
-tempLine, = tempax.plot(timeSamples, tempSamples, color='blue')
-humidLine, = humidax.plot(timeSamples, humidSamples, color = 'red')
-
-# Add labels
-plt.title('Temperature and Hunidity')
-plt.xlabel('Samples')
-tempax.set_ylabel('Temperature (deg C)')
-humidax.set_ylabel('Humidity (%)')
-
-def read_temp():
-    temp_c = random.randint(10, 40)
-
-    return temp_c
-
-def read_humid():
-    humid = random.randint(20, 25)
-
-    return humid
 
 # This function is called periodically from FuncAnimation
-def animate(i, tempSamples, humidSamples):
+def animate(i, tempSamples, humidSamples, timeLen, tempLine, humidLine):
 
-
-    temp_c =
-    humid =
+    temp_c = BLE_receiver.Temp
+    humid = BLE_receiver.Humid
     # Add y to list
     tempSamples.append(temp_c)
     humidSamples.append(humid)
@@ -61,12 +31,61 @@ def animate(i, tempSamples, humidSamples):
 
     return tempLine, humidLine, 
 
+def plot ():
 
-# Set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig,
-    animate,
-    fargs=(tempSamples, humidSamples),
-    interval=50,
-    blit=True)
-plt.show()
+    # Parameters
+    timeLen = 200         # Number of points to display
+    tempRange = [24, 32]  # Range of possible Y values to display
+    humidRange = [40, 50]  # Range of possible Y values to display
 
+    # Create figure for plotting
+    fig = plt.figure()
+    tempax = fig.add_subplot(1, 1, 1)
+    humidax = tempax.twinx()
+    timeSamples = list(range(0, 200))
+    tempSamples = [0] * timeLen
+    humidSamples = [0] * timeLen
+    tempax.set_ylim(tempRange)
+    humidax.set_ylim(humidRange)
+
+    # Create a blank line. We will update the line in animate
+    tempLine, = tempax.plot(timeSamples, tempSamples, color='blue')
+    humidLine, = humidax.plot(timeSamples, humidSamples, color = 'red')
+
+    # Add labels
+    plt.title('Temperature and Hunidity')
+    plt.xlabel('Samples')
+    tempax.set_ylabel('Temperature (deg C)')
+    humidax.set_ylabel('Humidity (%)')
+
+
+    # Set up plot to call animate() function periodically
+    ani = animation.FuncAnimation(fig,
+        animate,
+        fargs=(tempSamples, humidSamples, timeLen, tempLine, humidLine),
+        interval=BLE_receiver.samplingRate*1000,
+        blit=True)
+    plt.show()
+
+def start_ploting():
+    plot_thread = threading.Thread(target=plot)
+    plot_thread.start()
+
+
+async def async_main(device, outputFile):
+    
+    await asyncio.gather(BLE_receiver.BLErx(device, outputFile))
+
+
+if __name__ == "__main__":
+    start_ploting()
+
+    DeviceDetected = asyncio.run(BLE_receiver.BLEconnect())
+
+    data_file = open("Data/output.csv", "w")
+    data_file.write("Temperature, Humidity\n")
+
+    asyncio.run(async_main(device=DeviceDetected, outputFile=data_file))
+    # plot()
+
+    data_file.close()
